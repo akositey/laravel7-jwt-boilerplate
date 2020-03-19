3ddf5f68ec9c304f5865ab672748221c387ad619
Chester Martinez <chestermartinez@gmail.com>
2020-03-20 07:49:26 +0800

    improved tests

--- tests/Feature/AuthTest.php
+++ tests/Feature/AuthTest.php
@@ -21,7 +21,7 @@ class AuthTest extends TestCase
      *Test successful registration with good credentials
     */
     public function test_register(){
-        $password = $this->faker->password;
+        $password = $this->faker->password(8,20);
         $goodUserData = [
             'name' => $this->faker->name,
             'email' => $this->faker->safeEmail,
@@ -30,6 +30,10 @@ class AuthTest extends TestCase
         ];
         //try to register
         $response = $this->json('POST',route('api.register'), $goodUserData);
+        //for debugging
+        if($response->status() !== 200){
+            dump($response->getContent());
+        }
         //Assert that it is successful
         $response->assertStatus(200);
         //check for token in the response
@@ -40,7 +44,7 @@ class AuthTest extends TestCase
      *Test registration with invalid email
     */
     public function test_register_with_invalid_email(){
-        $password = $this->faker->password;
+        $password = $this->faker->password(8,20);
         $badUserData = [
             'name' => $this->faker->name,
             'email' => 'notAValidEmail@',
@@ -49,6 +53,10 @@ class AuthTest extends TestCase
         ];
         //try to register
         $response = $this->json('POST',route('api.register'), $badUserData);
+        //for debugging
+        if($response->status() !== 422){
+            dump($response->getContent());
+        }
         //Assert that it is NOT successful
         $response->assertStatus(422);
     }
@@ -60,56 +68,105 @@ class AuthTest extends TestCase
         $badUserData = [
             'name' => $this->faker->name,
             'email' => $this->faker->safeEmail,
-            'password' => $this->faker->password,
-            'password_confirmation' => $this->faker->password, //diff from the first one
+            'password' => $this->faker->password(8,20),
+            'password_confirmation' => $this->faker->password(8,20), //diff from the first one
         ];
         //Send post request
         $response = $this->json('POST',route('api.register'), $badUserData);
+        //for debugging
+        if($response->status() !== 422){
+            dump($response->getContent());
+        }
         //Assert that it is NOT successful, because the password confirmation does not match
         $response->assertStatus(422);
     }
 
 
     /**
-     *Test successful login
+     *Test Successful login
     */
     public function test_login()
     {
-        $email = $this->faker->safeEmail;
-        $password = $this->faker->password;
-        factory(User::class)->create([
-            'email' => $email,
+        $password = $this->faker->password(8,20);
+        $user = factory(User::class)->create([
             'password' => bcrypt($password)
         ]);
 
         //Try to log in
         $response = $this->json('POST',route('api.authenticate'), [
-            'email' => $email,
+            'email' => $user->email,
             'password' => $password,
         ]);
+        //for debugging
+        if($response->status() !== 200){
+            dump($response->getContent());
+        }
         //Assert that it succeeded and received the token
         $response->assertStatus(200);
         $this->assertArrayHasKey('access_token',$response->json());
     }
 
     /**
-     *Test successful login
+     *Test Login with Bad Email format
     */
-    public function test_unsuccessful_login()
+    public function test_login_with_bad_email()
     {
-        $email = $this->faker->freeEmail;
-        $password = $this->faker->password;
-        factory(User::class)->create([
-            'email' => $email,
+        $password = $this->faker->password(8,20);
+        $user = factory(User::class)->create([
             'password' => bcrypt($password)
         ]);
 
         //Try to log in
         $response = $this->json('POST',route('api.authenticate'), [
-            'email' => str_replace('@',".", $email),
+            'email' => str_replace('@',".", $user->email),
             'password' => $password,
         ]);
-        //Assert that it did NOT succeeded and received the token
+        //for debugging
+        if($response->status() !== 422){
+            dump($response->getContent());
+        }
+        //Assert that it did NOT succeed because email is invalid
         $response->assertStatus(422);
+    }
+
+    /**
+     *Test login with wrong password
+    */
+    public function test_login_with_wrong_email()
+    {
+        $password = $this->faker->password(8,20);
+        $user = factory(User::class)->create([
+            'password' => bcrypt($password)
+        ]);
+        //Try to log in
+        $response = $this->json('POST',route('api.authenticate'), [
+            'email' => "test".$user->email,
+            'password' => $password,
+        ]);
+        //for debugging
+        if($response->status() !== 401){
+            dump($response->getContent());
+        }
+        //Assert that it did NOT succeed because email is invalid
+        $response->assertStatus(401);
+    }
+
+    /**
+     *Test login with wrong password
+    */
+    public function test_login_with_wrong_password()
+    {
+        $user = factory(User::class)->create();
+        //Try to log in
+        $response = $this->json('POST',route('api.authenticate'), [
+            'email' => $user->email,
+            'password' => $this->faker->password(8,20),
+        ]);
+        //for debugging
+        if($response->status() !== 401){
+            dump($response->getContent());
+        }
+        //Assert that it did NOT succeed because email is invalid
+        $response->assertStatus(401);
     }
 }
